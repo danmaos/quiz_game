332cd423de9f86dc142b61d54000701829a3d768
dan <daniyar.f96@gmail.com>
2022-11-14 18:49:47 +0600

    first commit

---
+++ quiz_game.py
@@ -0,0 +1,14 @@
+questions = {
+    "Who created Python?: ": "A",
+    "What year was Python created?: ": "B",
+    "Python is tributed to which comedy group?: ": "C",
+    "Is the Earth round?: ": "A"
+}
+
+options = [["A. Guido van Rossum", "B. Elon Musk", "C. Bill Gates", "D. Mark Zuckerburg"],
+           ["A. 1989", "B. 1991", "C. 2000", "D. 2016"],
+           ["A. Lonely Island", "B. Smosh", "C. Monty Python", "D. SNL"],
+           ["A. True", "B. False", "C. sometimes", "D. What's Earth?"]]
+
+def new_game():
+    
